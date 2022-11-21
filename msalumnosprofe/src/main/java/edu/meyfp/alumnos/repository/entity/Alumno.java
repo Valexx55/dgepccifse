a1b046462f0e772eb2104dca317e436cb57d9602
vale <valexx55@gmai.com>
2022-11-21 12:37:24 +0100

    Alumno Entity

--- msalumnosprofe/src/main/java/edu/meyfp/alumnos/repository/entity/Alumno.java
+++ msalumnosprofe/src/main/java/edu/meyfp/alumnos/repository/entity/Alumno.java
@@ -1,10 +1,109 @@
 package edu.meyfp.alumnos.repository.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "alumnos")
 public class Alumno {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//autoinc en Mysql
+	private Long id;
+	
+
+	private String nombre;
+	private String apellido;
+	private String email;
+	private int edad;
+	
+	@Column(name = "creado_en")
+	@Temporal(TemporalType.TIMESTAMP)//represento la fecha con detalle de milisegundos
+	private Date creadoEn;
+	
+	@PrePersist//anbtes de que se inserte un alumno
+	private void generarFechaCreacion ()
+	{
+		this.creadoEn = new Date();//obtener la fecha actual
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public Date getCreadoEn() {
+		return creadoEn;
+	}
+
+	public void setCreadoEn(Date creadoEn) {
+		this.creadoEn = creadoEn;
+	}
+
+	public Alumno(Long id, String nombre, String apellido, String email, int edad, Date creadoEn) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.edad = edad;
+		this.creadoEn = creadoEn;
+	}
+	
+	public Alumno() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Alumno [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email + ", edad="
+				+ edad + ", creadoEn=" + creadoEn + "]";
+	}
+	
+	
 
 }
