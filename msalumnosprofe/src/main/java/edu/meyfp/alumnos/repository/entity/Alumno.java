4276229494c4736236818d1e3e15eac685ece0a5
vale <valexx55@gmai.com>
2022-11-21 11:24:07 +0100

    v1

---
+++ msalumnosprofe/src/main/java/edu/meyfp/alumnos/repository/entity/Alumno.java
@@ -0,0 +1,10 @@
+package edu.meyfp.alumnos.repository.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "alumnos")
+public class Alumno {
+
+}
---
+++ msalumnosprofe/src/test/java/edu/meyfp/alumnos/MsalumnosprofeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.meyfp.alumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsalumnosprofeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
