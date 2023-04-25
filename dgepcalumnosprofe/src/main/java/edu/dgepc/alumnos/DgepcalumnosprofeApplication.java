94a7588610cca19b5edbea786dc1c5884c084949
vale <valexx55@gmai.com>
2023-04-25 09:51:15 +0200

    v1

---
+++ dgepcalumnosprofe/src/main/java/edu/dgepc/alumnos/DgepcalumnosprofeApplication.java
@@ -0,0 +1,13 @@
+package edu.dgepc.alumnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgepcalumnosprofeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgepcalumnosprofeApplication.class, args);
+	}
+
+}
---
+++ dgepcalumnosprofe/src/test/java/edu/dgepc/alumnos/DgepcalumnosprofeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dgepc.alumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgepcalumnosprofeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
