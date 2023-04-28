425f8ec8c5ab4b992c1a6a74295055f1d2a4ca71
vale <valexx55@gmai.com>
2023-04-28 10:43:11 +0200

    zuul

--- dgepcalumnosprofe/src/main/java/edu/dgepc/alumnos/DgepcalumnosprofeApplication.java
+++ dgepcalumnosprofe/src/main/java/edu/dgepc/alumnos/DgepcalumnosprofeApplication.java
@@ -2,10 +2,21 @@ package edu.dgepc.alumnos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DgepcalumnosprofeApplication {
 
+	/**
+	 * 
+	 * Para configurar Eureka cliente:
+	 * 
+	 * 1) ADD dependencia 
+	 * 2) Incluyo la anotacion @EnableEurekaClient
+	 * 3) Configurar propiedades (dir servidor eureka)
+	 */
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DgepcalumnosprofeApplication.class, args);
 	}
---
+++ servidorgatewayprofe/src/test/java/edu/dgepc/gateway/ServidorgatewayprofeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dgepc.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorgatewayprofeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
